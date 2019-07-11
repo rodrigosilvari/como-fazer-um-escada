7402e8ac33e3340d6f879fcdbdb27ebe984ecad2
Isabele Prieto Himeno <52426897+Prietoisa@users.noreply.github.com>
2019-07-11 11:50:56 -0300

    codigo da escada

---
+++ script.js
@@ -0,0 +1,11 @@
+let lances = Number(prompt("lances"));
+let pergunta = prompt("material");
+
+let material = "";
+let i = 0;
+while(i < lances){    
+    material += pergunta;
+    console.log(material);
+    i++;
+
+}
